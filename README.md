3628f5b7f7b91b62f85d45e21d9bc4c44b583433
dlbr <mail@daliborgogic.com>
2019-01-02 15:27:37 +0100

    schema.org organization

--- README.md
+++ README.md
@@ -2,4 +2,38 @@
 > Progressive Web App (PWA) 
 <img src="https://i.imgur.com/7Nc7s8x.png" width="375">
 
+### Environment Variables
 
+| Key              | Description                                             |
+|:-----------------|:--------------------------------------------------------|
+| ```CMS_DOMAIN``` | Content Menagement System (CMS) Top Level Domain (TLD). |
+| ```APP_DOMAIN``` | Application Top Level Domain (TLD).                     |
+
+### Usage
+
+```bash
+# Install a package, and any packages that it depends on:
+$ npm i
+
+# Set Environment Variables:
+$ cat > .env << EOL
+CMS_DOMAIN=
+APP_DOMAIN=
+EOL
+
+# Launch a development server on localhost:3000 with hot-reloading
+$ npm run dev
+
+# Build application with webpack
+$ npm run build
+
+# Start the server in production mode
+$ npm start
+
+# Or start the server with process manager
+$ pm2 start process.json
+```
+
+### License
+[MIT](https://github.com/daliborgogic/sindikat/blob/master/LICENSE)
+Copyright (c) 2018-2019, Dalibor Gogic
--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,5 +1,39 @@
 import {} from 'dotenv/config'
 const { CMS_DOMAIN, APP_DOMAIN } = process.env
+const schema = {
+  '@context': 'http://schema.org/',
+  "@type": "Organization",
+  "name": "###",
+  "legalName": "###",
+  "description": "###",
+  "url": APP_DOMAIN,
+  "logo": APP_DOMAIN + '/logo.png',
+  "email": 'support@' + APP_DOMAIN,
+  "founder": {
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "givenName": "###",
+    "familyName": "###",
+    "gender": "Male",
+    "jobTitle": "###",
+    "image": APP_DOMAIN + "/founder.png"
+  },
+  "foundingDate": "2015-11-03T12:00:00-08:00",
+  "foundingLocation": {
+    "@context": "http://schema.org",
+    "@type": "Place",
+    "address": {
+      "@context": "http://schema.org",
+      "@type": "PostalAddress",
+      "addressLocality": "San Francisco",
+      "addressRegion": "CA"
+    }
+  },
+  "sameAs": [
+    "https://twitter.com/###",
+    "https://www.facebook.com/###"
+  ]
+}
 
 export default {
   modern: true,
@@ -11,9 +45,15 @@ export default {
 
   head: {
     htmlAttrs: { lang: 'sr', },
+    title: '###',
+    titleTemplate: '%s - ###',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    // __dangerouslyDisableSanitizers: ['script'],
+    script: [
+      { type: 'application/ld+json', innerHTML: JSON.stringify(schema, null, 0) }
     ]
   },
 
